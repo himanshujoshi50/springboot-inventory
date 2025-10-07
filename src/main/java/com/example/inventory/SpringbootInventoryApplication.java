55e96bb94e387de12e5a9afe3e467657f4d99269
Himanshu Joshi <your-email@example.com>
2025-10-07 06:07:03 +0000

    Initial Spring Boot app with Dockerfile and buildspec

---
+++ src/main/java/com/example/inventory/SpringbootInventoryApplication.java
@@ -0,0 +1,13 @@
+package com.example.inventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootInventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootInventoryApplication.class, args);
+	}
+
+}
